1de59275a04adba708294010de2b637d39e4db51
IndhumathiParamasivam <87922656+IndhumathiParamasivam@users.noreply.github.com>
2021-07-31 15:30:46 +0530

    uploading files for assignment provided on July 31st

---
+++ mainindex.js
@@ -0,0 +1,26 @@
+let Students = [
+    {name : "Adhi", gender : "male"},
+    {name : "Ahamed", gender : "Male"},
+    {name : "Dharshan", gender : "male"},
+    {name : "Indhumathi" , gender : "Female"},
+    {name : "Sridevi" , gender : "female"},
+    {name : "Abi", gender : "female"},
+    {name : "Vijay", gender : "male"},
+    {name : "Rani" , gender : "female"}
+];
+let mytable = "<table><th>Name</th><th>Gender</th>";
+Students.map((student) => mytable +="<tr><td>"+student.name+"</td><td>"+student.gender+"</td></tr>");
+mytable+="</table>";
+document.getElementById("stdAllDetail").innerHTML = mytable;
+let femaleStudents = Students.filter( (student) =>student.gender.toLowerCase()==="female").map( (femalestudent) => femalestudent.name);
+console.log("List of female students:");
+console.log(femaleStudents);
+document.getElementById("femalestudents").innerHTML = femaleStudents;
+let studentnamestartwithA = Students.filter( (student) =>student.name[0].toUpperCase()==='A').map( (student) => student.name);
+console.log("Students name start with 'A':");
+console.log(studentnamestartwithA);
+document.getElementById("namestartwithA").innerHTML = studentnamestartwithA;
+let countofmaleStudents = Students.filter( (student) => student.gender.toLowerCase()==="male").length;
+console.log("Count of Male Students:");
+console.log(countofmaleStudents);
+document.getElementById("malestudentscount").innerHTML = countofmaleStudents;
